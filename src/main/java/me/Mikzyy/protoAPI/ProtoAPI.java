52e3c1012e339c663f304035bee830172f6e4dfb
Mikaels132 <miroruuska045@gmail.com>
2026-01-03 14:51:17 +0200

    Initial commit

---
+++ src/main/java/me/Mikzyy/protoAPI/ProtoAPI.java
@@ -0,0 +1,17 @@
+package me.Mikzyy.protoAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProtoAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/Mikzyy/protoAPI/TestProtoAPI.java
@@ -0,0 +1,4 @@
+package me.Mikzyy.protoAPI;
+
+public class TestProtoAPI {
+}
