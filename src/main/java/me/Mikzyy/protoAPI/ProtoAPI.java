3b7d98a5396d1e81155b48fcc9112b6c644cb932
Mikaels132 <miroruuska045@gmail.com>
2026-01-03 15:41:38 +0200

    changes

--- src/main/java/me/Mikzyy/protoAPI/ProtoAPI.java
+++ src/main/java/me/Mikzyy/protoAPI/ProtoAPI.java
@@ -1,6 +1,8 @@
 package me.Mikzyy.protoAPI;
 
-public final class ProtoAPI {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProtoAPI extends JavaPlugin {
 
     public String getMessage() {
         return "Hello from ProtoAPI!";
